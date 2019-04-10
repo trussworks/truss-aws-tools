2ec08a747cb258c6095f50d8140823dd5ba00308
Chastity Blackwell <cblkwell@truss.works>
2019-04-10 14:16:13 -0700

    Adding testing for FindImagesToPurge; almost working

--- pkg/amiclean/ami_cleaner_test.go
+++ pkg/amiclean/ami_cleaner_test.go
@@ -3,39 +3,108 @@ package amiclean
 import (
 	"testing"
 	"time"
+	"reflect"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"go.uber.org/zap"
 )
 
-// Dummy data:
-//   - new AMI in master branch
-//   - new-ish AMI in development branch
-//   - old AMI in development branch
-//   - one must have multiple tags
-//   - one must have multiple EBS volumes
-
 var newMasterImage = &ec2.Image{
-	Description:
-	ImageId: "ami-11111111111111111"
-	CreationDate:
-	Tags:
+	Description: aws.String("New Master Image"),
+	ImageId: aws.String("ami-11111111111111111"),
+	CreationDate: aws.String("2019-03-31T21:04:57.000Z"),
+	Tags: []*ec2.Tag{
+		&ec2.Tag{ Key: aws.String("Branch"), Value: aws.String("master") },
+		&ec2.Tag{ Key: aws.String("Name"), Value: aws.String("newMasterImage") },
+	},
+	BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+		&ec2.BlockDeviceMapping{
+			DeviceName: aws.String("/dev/xvda"),
+			Ebs: &ec2.EbsBlockDevice{
+				SnapshotId: aws.String("snap-11111111111111111"),
+			},
+		},
+	},
 }
 
 var newishDevImage = &ec2.Image{
-	ImageId: "ami-22222222222222222"
+	Description: aws.String("Newish Dev Image"),
+	ImageId: aws.String("ami-22222222222222222"),
+	CreationDate: aws.String("2019-03-30T21:04:57.000Z"),
+	Tags: []*ec2.Tag{
+		&ec2.Tag{ Key: aws.String("Branch"), Value: aws.String("development") },
+		&ec2.Tag{ Key: aws.String("Name"), Value: aws.String("newishDevImage") },
+	},
+	BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+		&ec2.BlockDeviceMapping{
+			DeviceName: aws.String("/dev/xvda"),
+			Ebs: &ec2.EbsBlockDevice{
+				SnapshotId: aws.String("snap-22222222222222222"),
+			},
+		},
+		&ec2.BlockDeviceMapping{
+			DeviceName: aws.String("/dev/xvdb"),
+			Ebs: &ec2.EbsBlockDevice{
+				SnapshotId: aws.String("snap-22222222222222223"),
+			},
+		},
+	},
 }
 
 var oldDevImage = &ec2.Image{
-	ImageId: "ami-33333333333333333"
+	Description: aws.String("Old Dev Image"),
+	ImageId: aws.String("ami-33333333333333333"),
+	CreationDate: aws.String("2019-03-01T21:04:57.000Z"),
+	Tags: []*ec2.Tag{
+		&ec2.Tag{ Key: aws.String("Name"), Value: aws.String("oldDevImage") },
+		&ec2.Tag{ Key: aws.String("Branch"), Value: aws.String("development") },
+	},
+	BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+		&ec2.BlockDeviceMapping{
+			DeviceName: aws.String("/dev/xvda"),
+			Ebs: &ec2.EbsBlockDevice{
+				SnapshotId: aws.String("snap-33333333333333333"),
+			},
+		},
+	},
 }
 
-// Need to test FindImagesToPurge with:
-// Branch: master, retention 30 days
-// Branch: development, retention 30 days
-// Branch: development, retention 1 day
-// Branch: !master, retention 1 day
+var testImages = []*ec2.Image{ newMasterImage, newishDevImage, oldDevImage }
 
-// Need to test GetIdsToProcess against dummy data and generate proper list of
-// AMI IDs and Snapshot IDs
+var now = time.Date(2019, 4, 1, 0, 0, 0, 0, time.UTC)
+
+func TestFindImagesToPurge(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	tables := []struct {
+		imageSet []*ec2.Image
+		Branch string
+		RetentionDays int
+		resultSet []*ec2.Image
+	}{
+		{ testImages, "master", 1, []*ec2.Image{} },
+		{ testImages, "development", 30, []*ec2.Image{ oldDevImage } },
+		{ testImages, "development", 1, []*ec2.Image{ newishDevImage, oldDevImage } },
+		{ testImages, "!master", 1, []*ec2.Image{ newishDevImage, oldDevImage } },
+	}
+
+	for _, table := range tables {
+		a := AMIClean{
+			Branch: table.Branch,
+			DryRun: true,
+			ExpirationDate: now.AddDate(0, 0, -int(table.RetentionDays)),
+			Logger: logger,
+			EC2Client: nil,
+		}
+
+		output := &ec2.DescribeImagesOutput{ Images: testImages }
+		result := a.FindImagesToPurge(output)
+		if !reflect.DeepEqual(result, table.resultSet) {
+			t.Errorf("ERROR: branch %v, retention %d days failed;\n\texpected: %v\n\tgot: %v",
+			table.Branch,
+			table.RetentionDays,
+			table.resultSet,
+			result)
+		}
+	}
+}
