48513763ca34ba267071742db25ab3eb1e38d56b
Chastity Blackwell <cblkwell@truss.works>
2019-06-25 12:46:48 -0700

    Adding basics for packerjanitor pkg and success testing

---
+++ pkg/packerjanitor/packer_janitor.go
@@ -0,0 +1,238 @@
+package packerjanitor
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"go.uber.org/zap"
+
+	"time"
+)
+
+const (
+	// RFC8601 is the date/time format used by AWS.
+	RFC8601 = "2006-01-02T15:04:05.000Z"
+)
+
+// PackerClean is a generic struct used for the various functions.
+type PackerClean struct {
+	Delete         bool
+	ExpirationDate time.Time
+	Logger         *zap.Logger
+	EC2Client      ec2iface.EC2API
+}
+
+// GetPackerInstances -- find all running instances that are Packer
+// builds older than X and returns them in a list
+func (p *PackerClean) GetPackerInstances() ([]*ec2.Instance, error) {
+	var output *ec2.DescribeInstancesOutput
+	// Instances Packer starts all have the Name tag "Packer Builder".
+	packerFilter := &ec2.Filter{
+		Name:   aws.String("tag:Name"),
+		Values: []*string{aws.String("Packer Builder")},
+	}
+	input := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{packerFilter},
+	}
+	output, err := p.EC2Client.DescribeInstances(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				p.Logger.Error("Encountered AWS error attempting to get instance list",
+					zap.Error(aerr),
+				)
+			}
+		} else {
+			p.Logger.Error("Error while attempting to get instance list",
+				zap.Error(err),
+			)
+			return nil, err
+		}
+	}
+
+	// The output gives us reservations; we need to get the actual
+	// instances out of them, and look to make sure they are older
+	// than the time we're looking for.
+	var instanceList = []*ec2.Instance{}
+
+	for _, reservation := range output.Reservations {
+		for _, instance := range reservation.Instances {
+			// We need to check if the instance is older
+			// than our expiration, because we can't do
+			// that comparison in a filter above. :/
+			instanceLaunchTime := *instance.LaunchTime
+			if instanceLaunchTime.Before(p.ExpirationDate) {
+				instanceList = append(instanceList, instance)
+			}
+		}
+	}
+
+	return instanceList, nil
+
+}
+
+// CleanTerminateInstance -- Terminates an instance and waits until it is
+// gone before returning.
+func (p *PackerClean) CleanTerminateInstance(instance *ec2.Instance) error {
+	terminateInput := &ec2.TerminateInstancesInput{
+		DryRun:      aws.Bool(!p.Delete),
+		InstanceIds: []*string{instance.InstanceId},
+	}
+	_, err := p.EC2Client.TerminateInstances(terminateInput)
+	if err != nil {
+		// Check to see if this was an AWS error
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			// If this was a dryrun operation, we want to
+			// log that we would have succeeded and keep
+			// going.
+			case "DryRunOperation":
+				p.Logger.Info("Would have terminated instance",
+					zap.String("instance-id", *instance.InstanceId),
+					zap.Error(aerr),
+				)
+			default:
+				p.Logger.Error("Encountered AWS Error attempting to terminate instance",
+					zap.String("instance-id", *instance.InstanceId),
+					zap.Error(aerr),
+				)
+				return aerr
+			}
+		} else {
+			p.Logger.Error("Error while attempting to terminate instance",
+				zap.String("instance-id", *instance.InstanceId),
+				zap.Error(err),
+			)
+			return err
+		}
+	}
+
+	// If we were able to terminate the instance, wait for it to be
+	// terminated. If we're in a dry run, just return right away.
+	if !p.Delete {
+		return nil
+	}
+
+	describeInput := &ec2.DescribeInstancesInput{
+		DryRun: aws.Bool(!p.Delete),
+		Filters: []*ec2.Filter{&ec2.Filter{
+			Name:   aws.String("instance-id"),
+			Values: []*string{instance.InstanceId},
+		},
+		},
+	}
+	err = p.EC2Client.WaitUntilInstanceTerminated(describeInput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				p.Logger.Error("Encountered AWS error waiting for instance to terminate",
+					zap.String("instance-id", *instance.InstanceId),
+					zap.Error(aerr),
+				)
+				return aerr
+			}
+		} else {
+			p.Logger.Error("Error while waiting for instance to terminate",
+				zap.String("instance-id", *instance.InstanceId),
+				zap.Error(err),
+			)
+			return err
+		}
+	}
+
+	// Once the instance has terminated, we can return a success.
+	return nil
+
+}
+
+// PurgePackerResource -- takes an instance, collects the key and SG
+// for it, terminates the instance, waits until it is dead, and then
+// deletes the key pair and security group.
+func (p *PackerClean) PurgePackerResource(instance *ec2.Instance) error {
+	// First, we need to terminate the instance and wait for it
+	// to die; we can't delete security groups if an instance is
+	// still running with it.
+	err := p.CleanTerminateInstance(instance)
+	if err != nil {
+		p.Logger.Error("Failed to terminate instance",
+			zap.String("instance-id", *instance.InstanceId),
+			zap.Error(err),
+		)
+		return err
+	}
+
+	// Now that the instance is terminated, let's clean up the
+	// keypair.
+	deleteKeyInput := &ec2.DeleteKeyPairInput{
+		DryRun:  aws.Bool(!p.Delete),
+		KeyName: instance.KeyName,
+	}
+	_, err = p.EC2Client.DeleteKeyPair(deleteKeyInput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			// If we were doing this as a dry run, just log
+			// that we would have deleted it and continue.
+			case "DryRunOperation":
+				p.Logger.Info("Would have deleted keypair",
+					zap.String("keypair", *instance.KeyName),
+					zap.Error(aerr),
+				)
+			default:
+				p.Logger.Error("Encountered AWS error while deleting keypair",
+					zap.String("keypair", *instance.KeyName),
+					zap.Error(aerr),
+				)
+				return aerr
+			}
+		} else {
+			p.Logger.Error("Error while attempting to delete keypair",
+				zap.String("keypair", *instance.KeyName),
+				zap.Error(err),
+			)
+			return err
+		}
+	}
+
+	// We should also clean up the security group used by the
+	// instance.
+	deleteSGInput := &ec2.DeleteSecurityGroupInput{
+		DryRun: aws.Bool(!p.Delete),
+		// There should only be a single security group for a
+		// Packer instance, so we can get the first one from
+		// this list.
+		GroupId: instance.SecurityGroups[0].GroupId,
+	}
+	_, err = p.EC2Client.DeleteSecurityGroup(deleteSGInput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			// Same thing here as the rest; if we are in
+			// a dryrun, handwave our success.
+			case "DryRunOperation":
+				p.Logger.Info("Would have deleted security group",
+					zap.String("security-group", *instance.SecurityGroups[0].GroupId),
+					zap.Error(aerr),
+				)
+			default:
+				p.Logger.Error("Encountered AWS error while deleting security group",
+					zap.String("security-group", *instance.SecurityGroups[0].GroupId),
+					zap.Error(aerr),
+				)
+				return aerr
+			}
+		} else {
+			p.Logger.Error("Error attempting to delete security group",
+				zap.String("security-group", *instance.SecurityGroups[0].GroupId),
+				zap.Error(err),
+			)
+			return err
+		}
+	}
+
+	return nil
+
+}
---
+++ pkg/packerjanitor/packer_janitor_test.go
@@ -0,0 +1,189 @@
+package packerjanitor
+
+import (
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"go.uber.org/zap"
+)
+
+// We set up a mock EC2Client so that we can mock API calls for our code.
+type mockEC2Client struct {
+	ec2iface.EC2API
+}
+
+// Setting the time "now" to be midnight on 1 July 2019
+var now = time.Date(2019, 7, 1, 0, 0, 0, 0, time.UTC)
+
+var logger, _ = zap.NewProduction()
+
+// This is a Packer instance that is a day old and should be culled in
+// our tests.
+var packerInstanceOld = &ec2.Instance{
+	Tags: []*ec2.Tag{
+		&ec2.Tag{Key: aws.String("Name"), Value: aws.String("Packer Builder")},
+	},
+	KeyName:    aws.String("packer_1234"),
+	LaunchTime: aws.Time(time.Date(2019, 6, 30, 0, 0, 0, 0, time.UTC)),
+	InstanceId: aws.String("i-11111111111111111"),
+	SecurityGroups: []*ec2.GroupIdentifier{
+		&ec2.GroupIdentifier{GroupId: aws.String("sg-11111111111111111")},
+	},
+}
+
+// This is a Packer instance that is a month old and should be culled in
+// our tests.
+var packerInstanceAncient = &ec2.Instance{
+	Tags: []*ec2.Tag{
+		&ec2.Tag{Key: aws.String("Name"), Value: aws.String("Packer Builder")},
+	},
+	KeyName:    aws.String("packer_1234"),
+	LaunchTime: aws.Time(time.Date(2019, 5, 31, 0, 0, 0, 0, time.UTC)),
+	InstanceId: aws.String("i-22222222222222222"),
+	SecurityGroups: []*ec2.GroupIdentifier{
+		&ec2.GroupIdentifier{GroupId: aws.String("sg-22222222222222222")},
+	},
+}
+
+// This is a Packer instance that is only a minute old, so we shouldn't
+// be tossing it out in our tests.
+var packerInstanceNew = &ec2.Instance{
+	Tags: []*ec2.Tag{
+		&ec2.Tag{Key: aws.String("Name"), Value: aws.String("Packer Builder")},
+	},
+	KeyName:    aws.String("packer_6789"),
+	LaunchTime: aws.Time(time.Date(2019, 6, 30, 23, 59, 0, 0, time.UTC)),
+	InstanceId: aws.String("i-33333333333333333"),
+	SecurityGroups: []*ec2.GroupIdentifier{
+		&ec2.GroupIdentifier{GroupId: aws.String("sg-33333333333333333")},
+	},
+}
+
+// This is a helper function for testing whether two slices of instances
+// are the same (including order).
+func sliceEqual(a, b []*ec2.Instance) bool {
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Here we're mocking the DescribeInstances call that we'll be using in
+// the GetPackerInstances() function test; we are assuming that our
+// filtering (based on the tag) will work, so all this does is check that
+// the tag in the DescribeInstancesInput filter is set correctly.
+func (m *mockEC2Client) DescribeInstances(input *ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error) {
+	// There should really be only one filter we're using here, the
+	// tag for the Name being "Packer Builder". Making this
+	// assumption makes this code much simpler (we don't have to
+	// iterate through all the filters).
+	if *input.Filters[0].Name == "tag:Name" && *input.Filters[0].Values[0] == "Packer Builder" {
+		output := &ec2.DescribeInstancesOutput{
+			NextToken: aws.String(""),
+			// I'm splitting these up into two reservations
+			// to test the looping.
+			Reservations: []*ec2.Reservation{
+				&ec2.Reservation{Instances: []*ec2.Instance{packerInstanceOld}},
+				&ec2.Reservation{Instances: []*ec2.Instance{packerInstanceNew, packerInstanceAncient}},
+			},
+		}
+		return output, nil
+	}
+
+	output := &ec2.DescribeInstancesOutput{
+		NextToken:    aws.String(""),
+		Reservations: []*ec2.Reservation{},
+	}
+
+	return output, nil
+}
+
+// With the following functions, we're just looking to make sure we're
+// using the right inputs and outputs, and that we're not getting errors.
+// For a successful test, then, we can just have these be pretty dumb.
+func (m *mockEC2Client) TerminateInstances(input *ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error) {
+	return nil, nil
+}
+
+func (m *mockEC2Client) WaitUntilInstanceTerminated(input *ec2.DescribeInstancesInput) error {
+	return nil
+}
+
+func (m *mockEC2Client) DeleteKeyPair(input *ec2.DeleteKeyPairInput) (*ec2.DeleteKeyPairOutput, error) {
+	return nil, nil
+}
+
+func (m *mockEC2Client) DeleteSecurityGroup(input *ec2.DeleteSecurityGroupInput) (*ec2.DeleteSecurityGroupOutput, error) {
+	return nil, nil
+}
+
+// This is a helper function to generate a new PackerClean object;
+// we're doing this so that creating a new instance is easy if we
+// want to test with various mock EC2 clients.
+func testPackerClean(ec2client ec2iface.EC2API) PackerClean {
+	output := PackerClean{
+		Delete:         true,
+		ExpirationDate: now.Add(time.Hour * -4),
+		Logger:         logger,
+		EC2Client:      ec2client,
+	}
+
+	return output
+}
+
+// This function exercises the successful functioning of the
+// GetPackerInstances function.
+func TestGetPackerInstancesSuccess(t *testing.T) {
+	// The resultSet we're looking here is all the Packer instances
+	// except the new one, which should get filtered out by the
+	// ExpirationDate in GetPackerInstances.
+	resultSet := []*ec2.Instance{packerInstanceOld, packerInstanceAncient}
+
+	p := testPackerClean(&mockEC2Client{})
+
+	testSet, err := p.GetPackerInstances()
+
+	if err != nil {
+		t.Errorf("ERROR: GetPackerInstances threw error during successful test")
+	}
+
+	if !sliceEqual(testSet, resultSet) {
+		t.Errorf("ERROR: GetPackerInstances failed successful test;\n\texpected: %v,\n\tgot: %v",
+			resultSet, testSet,
+		)
+	}
+}
+
+// This function exercises the successful functioning of the
+// CleanTerminateInstance function.
+func TestCleanTerminateInstanceSuccess(t *testing.T) {
+	p := testPackerClean(&mockEC2Client{})
+	err := p.CleanTerminateInstance(packerInstanceOld)
+	if err != nil {
+		t.Errorf("ERROR: CleanTerminateInstance threw error during successful test")
+	}
+}
+
+// This function exercises the successful functioning of the
+// PurgePackerResource function.
+func TestPurgePackerResourceSuccess(t *testing.T) {
+	p := testPackerClean(&mockEC2Client{})
+	err := p.PurgePackerResource(packerInstanceOld)
+	if err != nil {
+		t.Errorf("ERROR: PurgePackerResource threw error during successful test")
+	}
+}
