31649aa3484c4c2cbc8ae40487ae1c3377966a07
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2020-04-25 21:15:19 -0700

    Use cobra

--- cmd/ecr-scan/main.go
+++ cmd/ecr-scan/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/service/ecr"
-	"github.com/spf13/pflag"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/trussworks/truss-aws-tools/internal/aws/session"
 	"github.com/trussworks/truss-aws-tools/pkg/ecrscan"
@@ -22,15 +24,19 @@ func makeECRClient(region, profile string) *ecr.ECR {
 	return ecrClient
 }
 
-func HandleRequest(ctx context.Context, target ecrscan.Target) (string, error) {
+func evaluateImage() (string, error) {
 	evaluator := ecrscan.Evaluator{
 		MaxScanAge: config.GetInt("maxScanAge"),
 		Logger:     logger,
 		ECRClient:  makeECRClient(config.GetString("region"), config.GetString("profile")),
 	}
+	target := ecrscan.Target{
+		Repository: config.GetString("repository"),
+		ImageTag:   config.GetString("tag"),
+	}
 	scanResult, err := evaluator.Evaluate(&target)
 	if err != nil {
-		logger.Error("Error evaluating target image")
+		logger.Error("Error evaluating target image", zap.Error(err))
 		return "", err
 	}
 	logger.Info("Scan result",
@@ -39,7 +45,42 @@ func HandleRequest(ctx context.Context, target ecrscan.Target) (string, error) {
 	return scanResult.Score, nil
 }
 
-func main() {
+func HandleRequest(ctx context.Context, target ecrscan.Target) (string, error) {
+	config.Set("repository", target.Repository)
+	config.Set("tag", target.ImageTag)
+	return evaluateImage()
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "ecr-scan",
+	Short: "ecr-scan is an application for analyzing ECR scan findings",
+	Long:  "ecr-scan is an application for analyzing ECR scan findings",
+	Run: func(cmd *cobra.Command, args []string) {
+		// declare error so that call to zap.NewProduction() will use
+		// logger declared above
+		var err error
+		logger, err = zap.NewProduction()
+		if err != nil {
+			log.Fatalf("could not initialize zap logger: %v", err)
+		}
+		defer logger.Sync()
+
+		if config.GetBool("lambda") {
+			lambda.Start(HandleRequest)
+		} else {
+			evaluateImage()
+		}
+	},
+}
+
+func execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
 	config = viper.New()
 	config.AutomaticEnv()
 	config.BindEnv("repository", "ECR_REPOSITORY")
@@ -48,44 +89,17 @@ func main() {
 	config.BindEnv("maxScanAge", "MAX_SCAN_AGE")
 	config.BindEnv("profile", "AWS_PROFILE")
 	config.BindEnv("region", "AWS_REGION")
-	pflag.StringP("repository", "r", "", "ECR repository where the image is located")
-	pflag.StringP("tag", "t", "", "Image tag to retrieve findings for")
-	pflag.Bool("lambda", false, "Run as Lambda function")
-	pflag.IntP("maxScanAge", "m", 24, "Maximum allowed age for image scan (hours)")
-	pflag.String("profile", "", "The AWS profile to use")
-	pflag.String("region", "", "The AWS region to use")
-	pflag.Parse()
-	config.BindPFlags(pflag.CommandLine)
+	rootCmd.Flags().StringP("repository", "r", "", "ECR repository where the image is located")
+	rootCmd.Flags().StringP("tag", "t", "", "Image tag to retrieve findings for")
+	rootCmd.Flags().Bool("lambda", false, "Run as Lambda function")
+	rootCmd.Flags().IntP("maxScanAge", "m", 24, "Maximum allowed age for image scan (hours)")
+	rootCmd.Flags().String("profile", "", "The AWS profile to use")
+	rootCmd.Flags().String("region", "", "The AWS region to use")
+	config.BindPFlags(rootCmd.Flags())
+	config.SetDefault("lambda", false)
+	config.SetDefault("maxScanAge", 24)
+}
 
-	// declare error so that call to zap.NewProduction() will use
-	// logger declared above
-	var err error
-	logger, err = zap.NewProduction()
-	if err != nil {
-		log.Fatalf("could not initialize zap logger: %v", err)
-	}
-	defer logger.Sync()
-
-	if config.IsSet("repository") && config.IsSet()
-
-	if config.GetBool("lambda") {
-		lambda.Start(HandleRequest)
-	} else {
-		evaluator := ecrscan.Evaluator{
-			MaxScanAge: config.GetInt("maxScanAge"),
-			Logger:     logger,
-			ECRClient:  makeECRClient(config.GetString("region"), config.GetString("profile")),
-		}
-		target := ecrscan.Target{
-			Repository: config.GetString("repository"),
-			ImageTag:   config.GetString("tag"),
-		}
-		scanResult, err := evaluator.Evaluate(&target)
-		if err != nil {
-			logger.Fatal("Error evaluating target image", zap.Error(err))
-		}
-		logger.Info("Scan result",
-			zap.String("score", scanResult.Score),
-			zap.Int("totalFindings", scanResult.TotalFindings))
-	}
+func main() {
+	execute()
 }
