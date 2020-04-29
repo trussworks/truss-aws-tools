d519b887b7733d03e75ddfaf271386b71b53d102
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2020-04-28 17:45:24 -0700

    Initial documentation for ecrscan package

--- pkg/ecrscan/ecr_scan.go
+++ pkg/ecrscan/ecr_scan.go
@@ -14,21 +14,28 @@ import (
 
 var validate *validator.Validate
 
+// Target specifies the ECR image to retrieve scan findings for
 type Target struct {
 	Repository string `json:"repository" validate:"required"`
 	ImageTag   string `json:"imageTag" validate:"required"`
 }
 
+// Report contains scan finding information to be returned to the caller
 type Report struct {
 	TotalFindings int `json:"totalFindings"`
 }
 
+// Evaluator orchestrates the retrieval and analysis of image scan findings
 type Evaluator struct {
 	MaxScanAge int
 	Logger     *zap.Logger
 	ECRClient  ecriface.ECRAPI
 }
 
+// Evaluate returns vulnerability scan information for a specified ECR image.
+// The function queries ECR for the image's current scan findings. If the most
+// recent scan is older than MaxScanAge, the function initiates a new scan. The
+// function response includes a Report of the image's total scan findings.
 func (e *Evaluator) Evaluate(target *Target) (*Report, error) {
 	validate = validator.New()
 	err := validate.Struct(target)
@@ -60,6 +67,7 @@ func (e *Evaluator) Evaluate(target *Target) (*Report, error) {
 	}, nil
 }
 
+// scan initiates an ECR vulnerability scan for an image.
 func (e *Evaluator) scan(target *Target) error {
 	e.Logger.Info("Scanning image")
 	_, err := e.ECRClient.StartImageScan(&ecr.StartImageScanInput{
@@ -75,8 +83,12 @@ func (e *Evaluator) scan(target *Target) error {
 	return err
 }
 
+// getImageFindings returns image scan findings for a target image. It will wait
+// until an image scan is complete and will initiate a scan if an existing scan
+// is not found.
 func (e *Evaluator) getImageFindings(target *Target) (*ecr.DescribeImageScanFindingsOutput, error) {
 	var scanFindings *ecr.DescribeImageScanFindingsOutput
+	// Wrap the query in a retry
 	err := retry.Do(
 		func() error {
 			result, err := e.ECRClient.DescribeImageScanFindings(
@@ -87,6 +99,9 @@ func (e *Evaluator) getImageFindings(target *Target) (*ecr.DescribeImageScanFind
 					RepositoryName: aws.String(target.Repository),
 				})
 			if err != nil {
+				// If the repo is not configured to scan on
+				// push, the image may not have an existing
+				// scan. In that scenario, initiate a scan.
 				var aerr *ecr.ScanNotFoundException
 				if errors.As(err, &aerr) {
 					e.Logger.Info("No scan found for image")
@@ -98,6 +113,8 @@ func (e *Evaluator) getImageFindings(target *Target) (*ecr.DescribeImageScanFind
 					return retry.Unrecoverable(errors.New("Unable to describe scan findings"))
 				}
 			}
+			// Check the scan status and drop out of the retry block
+			// if the scan is complete.
 			switch scanStatus := *result.ImageScanStatus.Status; scanStatus {
 			case ecr.ScanStatusFailed:
 				return retry.Unrecoverable(errors.New("Image scan failed"))
@@ -121,11 +138,20 @@ func (e *Evaluator) getImageFindings(target *Target) (*ecr.DescribeImageScanFind
 	return scanFindings, nil
 }
 
+// isOldScan returns true if the image scan was completed more than MaxScanAge
+// hours ago relative to the current time; false otherwise.
 func (e *Evaluator) isOldScan(findings *ecr.ImageScanFindings) bool {
 	scanTime := findings.ImageScanCompletedAt
 	return time.Since(*scanTime).Hours() > float64(e.MaxScanAge)
 }
 
+// calculateTotalFindings returns the number of findings in the image scan. By
+// default, the call to DescribeImageScanFindings
+// (https://docs.aws.amazon.com/sdk-for-go/api/service/ecr/#ECR.DescribeImageScanFindings)
+// returns a maximum of 100 results unless the MaxResults parameter is specified
+// (https://docs.aws.amazon.com/sdk-for-go/api/service/ecr/#DescribeImageScanFindingsInput).
+// Thus, rather than relying on the length of the ImageScanFinding slice, the
+// function calculates total findings based on the FindingSeverityCounts map.
 func (e *Evaluator) calculateTotalFindings(findings *ecr.ImageScanFindings) int {
 	counts := findings.FindingSeverityCounts
 	total := 0
